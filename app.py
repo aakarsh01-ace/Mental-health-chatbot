1f95908d255a5de6128bbb658412b611d74be92a
aakarsh01-ace <chaurasianidhi61082@gmail.com>
2024-06-06 22:47:39 +0530

    Update commit

--- app.py
+++ app.py
@@ -2,3 +2,31 @@ import pickle
 import nltk
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
+import json
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words = []
+classes = []
+documents = []
+ignore_word = ['?', '!']
+data_src = open('data.json').read()
+intents = json.loads(data_src)
+
+# step 1 would be to extract list of intents from a dictionary called intents
+for intent in intents['intents']:
+    # loop through all patterns in current intent
+    for pattern in intent['patterns']:
+        # split the patterns, that is, the sentences or phrases, into words and assign them to a variable
+        x = nltk.word_tokenize('patterns')
+        words.extends(x) # add the tokens from the current pattern to the "words" list. words is a list here that stores the tokenized words
+        # store tuples in a list that stores the tuples corresponding to each tag
+        documents.append((x, intent['tag']))
+        
+
+
+
